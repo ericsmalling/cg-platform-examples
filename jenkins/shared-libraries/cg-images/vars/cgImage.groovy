fa7fbbb367be71d9b9b8d157af3048843a7b7bf2
Eric Smalling <eric.smalling@chainguard.dev>
2026-05-12 14:09:03 -0500

    Update image digests
    Signed-off-by: Eric Smalling <eric.smalling@chainguard.dev>

--- jenkins/shared-libraries/cg-images/vars/cgImage.groovy
+++ jenkins/shared-libraries/cg-images/vars/cgImage.groovy
@@ -43,32 +43,32 @@ def call(String token) {
   def reg = env.PULL_REGISTRY ?: "cgr.dev/${env.CHAINGUARD_ORG}"
   def catalog = [
     'corretto-java17': [
-      build: 'maven:3-jdk17-dev@sha256:90e0bf8239086e814fc92090749f4f3b7b49a88107c655b0661509a2a4f2ee58',
-      test:  'amazon-corretto-jre:17-dev@sha256:46883ffbb2b5e99cf52cb124d9fced7b4e3740cacd46c1913bc2e970b7613353',
+      build: 'maven:3-jdk17-dev@sha256:2ef87b6d8b49cce6a4954346b20972ffee4f1259a4507e8f6f1b1abef180d5c0',
+      test:  'amazon-corretto-jre:17-dev@sha256:a34fe97ec1f505f8db6e64651c322b4c1f135816fc993d4e2a561380be0ad1a7',
     ],
     'adoptium-java8': [
-      build: 'maven:3-jdk8-dev@sha256:9df83d553cef9c7bc3daabd51fcf993478516339c525eca0307f780f2ed3743a',
-      test:  'adoptium-jre:adoptium-openjdk-8-dev@sha256:b06752d6781be20ef0ca4bc50459ef1c7518acec79122e4583ee9a24f6ed396a',
+      build: 'maven:3-jdk8-dev@sha256:9e1ecf89cc12c54c3297451d5883ce4b113d58ef841eafefe4cc9d1c9af1b0a4',
+      test:  'adoptium-jre:adoptium-openjdk-8-dev@sha256:e612d3dd7d0b96686e76ea2b46520675d60f8e7b0c77697ca38cfb8b1ede03db',
     ],
     'openjdk21': [
-      build: 'jdk:openjdk-21-dev@sha256:22a0cda00ee2980c4e1c7c35f7ddfa4391e06a2a8806887bffb5e5283f149ee1',
-      test:  'jre:openjdk-21-dev@sha256:c5bc29d25e88d244e7caaa344285007da619cd0bf83c350989398e1553775ecf',
+      build: 'jdk:openjdk-21-dev@sha256:916c9ee559c9df5c5e535ce8bc76abd8f77dd5908764b84dc7e21193a90588eb',
+      test:  'jre:openjdk-21-dev@sha256:da97c2c594c613969b007d1822de612b8c163f52b9234ca6bb07dd89154c5fb9',
     ],
     'python-3.14': [
-      build:   'python:3.14-dev@sha256:aa69dcd8a8689f7584fd4e077a52c0f13812ec7f54ace6590ab31f4297b3129d',
-      runtime: 'python:3.14@sha256:0d0af6d76f7caf6b0d21015b66089bef7f017d062652a3b297f211d07e319ec4',
+      build:   'python:3.14-dev@sha256:d83b1ceb93ad3c461b19ce91aa1ec5de9390d072f9b1966176d1f1678851f860',
+      runtime: 'python:3.14@sha256:ab7b45ffefc935c7be5097638a1008458a6d1ea23ccc20870f35862e88c8eab6',
     ],
     'python-3.12': [
-      build:   'python:3.12-dev@sha256:6f0af8cc50dd3853ce3fb145874e2408c868ba50e7104691a43794efda509e57',
-      runtime: 'python:3.12@sha256:c989e9a79d89581d777a9249444ebf7a9a64835188fe958d18ee3f19a98d25e1',
+      build:   'python:3.12-dev@sha256:d016f148c8a82ccc93f57dc3ad1733c26dd8c7cde0ddac17fa9c13181377d24e',
+      runtime: 'python:3.12@sha256:ba3950f1f9075ec52f0bfaaa2a45c9956fabbfb36e27cfde8eda950dc56a603f',
     ],
     'node-22': [
-      build:   'node:22-dev@sha256:dd916e3ed5be3b662cb598ad02a9e8b31a5ab23964ac92db4f79378ffa9fccca',
-      runtime: 'node:22@sha256:63323818cad51c97855be7c45a5ff8933983658b0b88051f7800368bcf5b938c',
+      build:   'node:22-dev@sha256:2cc72f0d8f860b48193c268c5b1e1d13dbb0ace7d25effcee1159d018476ca4a',
+      runtime: 'node:22@sha256:fa8d7ff7a1ddd9236ca47d69809823698e0ffb8977dac80b9ab122fbfc6ad419',
     ],
     'node-25': [
-      build:   'node:25-dev@sha256:cea252f9844fcaf7a224f009201e202e956d316976fe95df3c3c51d78ba10187',
-      runtime: 'node:25-slim@sha256:c1c2a3206d54ce0d1e04f108de77429325fd50b3ad266f7fbf827536ffed0292',
+      build:   'node:25-dev@sha256:40f793e15f4e8454bd6f766bb095cdf2b31f55f2509824ce0728b46fd590b96c',
+      runtime: 'node:25-slim@sha256:1e196de14544a44d6cdab353e872ee7aa1ff812ff23edbe6627c954603bcd569',
     ],
   ]
   if (!catalog.containsKey(token)) {
